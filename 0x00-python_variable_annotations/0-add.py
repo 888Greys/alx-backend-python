a04733e75cedd8b3825447530dadee48d4bb95da
888Greys <toxicgreys001@gmail.com>
2024-05-03 13:39:43 -0700

    Python variable annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Script for type-annotated function add that takes a float a
+"""
+
+
+def add(a: float, b: float) -> float:
+    """add two floats and return their sum.
+    """
+    return a + b
