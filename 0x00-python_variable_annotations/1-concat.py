2b12a8a4a74e2b3aba3fb67fafe459546c8eae17
888Greys <toxicgreys001@gmail.com>
2024-05-03 13:42:28 -0700

    Python variable annotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+""" Module that contains type-annotated function that takes concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenate two strings and return the result.
+    """
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+contains a type-annotated function that takes a float n as argument
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """return the floor of a float."""
+    return math.floor(n)
