2c4200b145b70e1dfd375d9408ade02bc8aa0331
888Greys <toxicgreys001@gmail.com>
2024-05-06 14:22:35 -0700

    Python Asynchronous

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+"""
+Asynchronous coroutine that takes in an integer
+"""
+
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Wait for some time
+    """
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
