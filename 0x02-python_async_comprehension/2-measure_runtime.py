c05f845bf4e8743873f6600cbae21bb8a1f0a486
888Greys <toxicgreys001@gmail.com>
2024-05-07 21:54:11 +0300

    Update

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ four times in parallel using asyncio.gather.
 measure_runtime should measure the total runtime and return it.
 
 Notice that the total runtime is roughly 10 seconds,
-explain it to yourself.
+explain it to yourself
 """
 
 
